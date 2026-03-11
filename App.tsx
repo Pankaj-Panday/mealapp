ecc96102a1d36f861832f6c284607ff1ab024681
Pankaj Panday <ppanday064@gmail.com>
2026-03-11 20:12:03 +0530

    feat: Integrate NativeWind for Tailwind CSS, React Native Vector Icons, and foundational React Navigation setup.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+
+import Ionicons from '@react-native-vector-icons/ionicons';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
+
+import './global.css';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -28,10 +26,18 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text
+        className="text-3xl font-bold underline"
+        style={{ marginTop: safeAreaInsets.top }}
+      >
+        Hello world!
+      </Text>
+
+      {/* ICON TEST */}
+      <View style={styles.iconRow}>
+        <Ionicons name="home" size={32} color="#4F46E5" />
+        <MaterialIcons name="favorite" size={32} color="red" />
+      </View>
     </View>
   );
 }
@@ -39,6 +45,13 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconRow: {
+    flexDirection: 'row',
+    gap: 20,
+    marginTop: 20,
   },
 });
 
